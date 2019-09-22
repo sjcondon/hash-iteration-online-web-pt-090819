f7c29fa1d38e2014340d9c01c068e074ae295037
sarah <s.j.condon01@gmail.com>
2019-09-22 16:39:32 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -4,9 +4,9 @@ suite_b: "Seymour Hoffman",
 suite_c: "Alfred Tennyson", 
 suite_d: "Charlie Chaplin", 
 suite_e: "Crumpet the Elf"
-# }
+ }
 
-def select_winner
+def select_winner(passenger)
   winner = ""
   passenger.each do |suite,name|
     if suite == :suite_a && name.start_with?

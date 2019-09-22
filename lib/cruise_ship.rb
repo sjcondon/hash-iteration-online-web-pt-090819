de33d2b33072682a936fb4cab60bc1e7690e4e3e
sarah <s.j.condon01@gmail.com>
2019-09-22 16:35:33 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,15 +16,5 @@ def select_winner(passengers)
 end
 
 winner
-end
 
-birthday_kids = {
-    "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
-}
 
-def happy_birthday(birthday_kids)
-  birthday.kids.each do |kids_name,age|
-    puts 
-}
